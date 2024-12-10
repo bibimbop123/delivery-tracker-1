b508de25fbc8a2759ecb736a8247c69f9c4f5050
Brian Kim <brianhkim13@gmail.com>
2024-12-10 19:48:39 +0000

    added deliveries controller and model and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
+  resources :deliveries
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # READ
+  root "deliveries#index"
+
+  # CREATE
+  post("/insert_delivery", { :controller => "deliveries", :action => "create" })
+
+
+  get("/deliveries/:path_id", { :controller => "deliveries", :action => "show" })
+
+  # UPDATE
+
+  post("/modify_delivery/:path_id", { :controller => "deliveries", :action => "update" })
+
+  # DELETE
+  get("/delete_delivery/:path_id", { :controller => "deliveries", :action => "destroy" })
+
+  #------------------------------
+
 end
