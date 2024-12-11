9a8b02d943b3b6e54e490bb1c7beec71172e75a8
Brian Kim <brianhkim13@gmail.com>
2024-12-11 21:52:44 +0000

    done with delivery tracker

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -4,13 +4,18 @@ class DeliveriesController < ApplicationController
     matching_deliveries = Delivery.all
 
     @list_of_deliveries = Delivery.where(user_id: current_user.id, arrived: false)
-    .order(created_at: :desc)
+    .order(created_at: :desc).where(:arrived => false)
+    @list_of_received = matching_deliveries.order({
+      :updated_at => :desc }).where(:arrived => true)
     
     render({ :template => "deliveries/index" })
   end
    def deliveries
     matching_deliveries = Delivery.all
-    @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
+ 
+    @list_of_deliveries = matching_deliveries.order({ :supposed_to_arrive_on => :desc }).where(:arrived => false)
+    @list_of_received = matching_deliveries.order({
+      :updated_at => :desc }).where(:arrived => true)
     render({ :template => "deliveries/index" })
   end
 
@@ -43,24 +48,20 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = true
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Marked as received."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
-
+  
   def destroy
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
-
+  
     the_delivery.destroy
 
     redirect_to("/", { :notice => "Deleted."} )
